e7f2f286dae9a7b6015c66be6e449f07a15c4849
seddikKy <60516560+seddikKy@users.noreply.github.com>
2023-11-30 20:57:09 +0100

    Update test_product.py

--- backend/test_product.py
+++ backend/test_product.py
@@ -1,36 +1,36 @@
 import unittest
-from product import ProductType, Product
+from product import  DigitalProduct, PackageProduct, PromotionalProduct
 
 
 class TestProduct(unittest.TestCase):
 
     def test_price_screen(self):
-        product = Product(ProductType.Digital, "Digital", 0, 0, False)
-        assert product.price() == 1000
+        digital_product = DigitalProduct(name="Digital", number_of_assets=0, discount=0, free=False)
+        assert digital_product.price() == 1000
 
     def test_price_package_one_asset(self):
-        product = Product(ProductType.Package, "Package - 1 asset", 1, 0, False)
-        assert product.price() == 1000
+        package_product = PackageProduct(name="Package - 1 asset", number_of_assets=1, discount=0, free=False)
+        assert package_product.price() == 1000
 
     def test_price_package_five_assets(self):
-        product = Product(ProductType.Package, "Package - 5 assets", 5, 0, False)
-        assert product.price() == 4500
+        package_product = PackageProduct(name="Package - 5 assets", number_of_assets=5, discount=0, free=False)
+        assert package_product.price() == 4500
 
     def test_price_package_zero_asset(self):
-        product = Product(ProductType.Package, "Package - 0 assets", 0, 0, False)
-        assert product.price() == 100
+        package_product = PackageProduct(name="Package - 0 asset", number_of_assets=0, discount=0, free=False)
+        assert package_product.price() == 100
 
     def test_price_promotional_10_percent(self):
-        product = Product(ProductType.Promotional, "Promotional - 10 percent", 0, 10, False)
-        assert product.price() == 900
+        promotional_product = PromotionalProduct(name="Promotional - 10 percent", number_of_assets=0, discount=10, free=False)
+        assert promotional_product.price() == 900
 
     def test_price_promotional_60_percent(self):
-        product = Product(ProductType.Promotional, "Promotional - 60 percent", 0, 60, False)
-        assert product.price() == 500
+        promotional_product = PromotionalProduct(name="Promotional - 60 percent", number_of_assets=0, discount=60, free=False)
+        assert promotional_product.price() == 500
 
     def test_price_promotional_free(self):
-        product = Product(ProductType.Promotional, "Promotional - Free", 0, 60, True)
-        assert product.price() == 0
+        promotional_product = PromotionalProduct(name="Promotional - 60 percent", number_of_assets=0, discount=60, free=True)
+        assert promotional_product.price() == 0
 
 
 if __name__ == '__main__':
